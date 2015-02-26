85485e569116ffc684d6d1640623a489e99abb56
Okal Otieno <okal@justokal.com>
2015-02-26 17:44:59 +0300

    OO: Override queryFixtures in Adapter

--- src/javascript/application.js
+++ src/javascript/application.js
@@ -1,3 +1,8 @@
 window.DFIDDashboard = Ember.Application.create();
 
-DFIDDashboard.ApplicationAdapter = DS.FixtureAdapter.extend();
+DFIDDashboard.ApplicationAdapter = DS.FixtureAdapter.extend({
+    queryFixtures: function(fixtures, query, type) {
+        var key = Ember.keys(query)[0];
+        return fixtures.filterBy(key, query[key]);
+    }
+});
