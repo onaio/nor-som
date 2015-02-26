360a73f971406b774ad0efe65c07fcc67e00882f
Okal Otieno <okal@justokal.com>
2015-02-26 20:39:13 +0300

    OO: Use DFIDDashboard.organizations as moel for Organization/s routes

--- src/javascript/application.js
+++ src/javascript/application.js
@@ -1,5 +1,31 @@
 window.DFIDDashboard = Ember.Application.create();
 
+DFIDDashboard.organizations = [
+    {
+        id: 1,
+        name: 'BRiCS',
+        slug: 'brics',
+        datasets: [
+            {
+                name: 'BRiCS Community Plans',
+                cartoVisualizationID: 'c15061cc-bb3c-11e4-a7da-0e9d821ea90d',
+                cartoTableName: 'Brics_community_plans_final',
+            }
+        ]
+
+    },
+    {
+        id: 2,
+        name: 'SNS',
+        slug: 'sns'
+    },
+    {
+        id: 3,
+        name: 'FAO',
+        slug: 'fao'
+    }
+]
+
 DFIDDashboard.ApplicationAdapter = DS.FixtureAdapter.extend({
     queryFixtures: function(fixtures, query, type) {
         var key = Ember.keys(query)[0];
