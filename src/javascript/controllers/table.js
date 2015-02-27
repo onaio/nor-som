8e0f0dd588b47914de2d82b1e5a5d3fc64e77e25
Okal Otieno <okal@justokal.com>
2015-02-27 15:40:21 +0300

    OO|ME: Restore ember table component Using 0.4.0

--- src/javascript/controllers/table.js
+++ src/javascript/controllers/table.js
@@ -1,7 +1,16 @@
 DFIDDashboard.TableController = Ember.Controller.extend({
     columns: Ember.computed('content.rawData', function() {
         if(!this.content.rawData[0]) { return []; }
-        return Object.keys(this.content.rawData[0]);
+        var headings = Object.keys(this.content.rawData[0]);
+        var columns = headings.map(function(entry) {
+            var columnDefinition = Ember.Table.ColumnDefinition.create({
+                headerCellName: entry,
+                textAlign: 'text-align-left',
+                contentPath: entry
+            });
+            return columnDefinition;
+        });
+        return columns;
     }),
     rows: Ember.computed('columns', function() {
         var controller = this
