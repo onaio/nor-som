117648d85cda566d70ea1200d819bbf8fd5b4da6
Okal Otieno <okal@justokal.com>
2015-02-27 14:32:40 +0300

    OO: Generate rows correctly

--- src/javascript/controllers/table.js
+++ src/javascript/controllers/table.js
@@ -1,20 +1,22 @@
 DFIDDashboard.TableController = Ember.Controller.extend({
     columns: Ember.computed('content.rawData', function() {
-        console.log(this.content.rawData[0]);
         if(!this.content.rawData[0]) { return []; }
         return Object.keys(this.content.rawData[0]);
     }),
-    rows: Ember.computed('content.rawData', function() {
+    rows: Ember.computed('columns', function() {
+        var controller = this
+        var keys = controller.get('columns')
         var rows = this.content.rawData.map(function(row) {
             var values = [];
-            for(key in this.columns) {
+            for(index in keys) {
+                var key = keys[index];
                 if(row.hasOwnProperty(key)) {
                     values.push(row[key]);
                 }
             }
             return values;
         })
-        console.log(rows);
+        return rows;
     }),
     name: Ember.computed('content.rawData', function() {
         return this.content.name;
