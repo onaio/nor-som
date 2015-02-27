e1b3b8ccee0cdf1964d3963e09e3ed61e96f8929
Okal Otieno <okal@justokal.com>
2015-02-27 10:06:17 +0300

    OO: Add other BRiCS dataset

--- src/javascript/application.js
+++ src/javascript/application.js
@@ -6,7 +6,16 @@ DFIDDashboard.datasets = [
         name: 'BRiCS Community Plans',
         slug: 'brics-community-plans',
         cartoVisualizationID: 'c15061cc-bb3c-11e4-a7da-0e9d821ea90d',
-        cartoTableName: 'Brics_community_plans_final',
+        cartoTableName: 'brics_community_plans_final',
+        rawData: []
+    },
+    {
+        organizationSlug: 'brics',
+        name: 'BRiCS Registration Data',
+        slug: 'brics-registration-data',
+        cartoVisualizationID: '8adc94f2-be35-11e4-a9de-0e0c41326911',
+        cartoTableName: 'brics_reg_data',
+        rawData: []
     }
 ]
 
