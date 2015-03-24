d45766b1a869ffbc150c8ef7be7c700832f48cf7
Okal Otieno <okal@justokal.com>
2015-03-24 17:16:03 +0300

    OO: Only execute query if it is available

--- src/javascript/router.js
+++ src/javascript/router.js
@@ -15,39 +15,40 @@ DFIDDashboard.VizRoute = Ember.Route.extend({
         var visualizations = organization.get('visualizations');
         visualizations.map(function(visualization) {
             var query = visualization.query;
-            DFIDDashboard.sql.execute(query)
-                .done(function(data) {
-                    var rows = data.rows;
-                    var columns = visualization.columnNames.map(function(columnName) {
-                        var column = [columnName];
-                        rows.map(function(row) {
-                            column.push(row[columnName]);
+            if (query) {
+                DFIDDashboard.sql.execute(query)
+                    .done(function(data) {
+                        var rows = data.rows;
+                        var columns = visualization.columnNames.map(function(columnName) {
+                            var column = [columnName];
+                            rows.map(function(row) {
+                                column.push(row[columnName]);
+                            });
+                            return column;
                         });
-                        return column;
-                    });
-                    console.log(columns);
-                    var chartData= {
-                        columns: columns,
-                        type: 'bar',
-                        groups: [
-                            visualization.columnNames
-                        ]
-                    };
-                    var chartAxis = {
-                        rotated: true,
-                        x: {
-                            type: 'category',
-                            categories: rows.map(function(row) { return row[visualization.categorizedBy]; })
-                        }
-                    };
-                    visualization.set('chartData', chartData);
-                    visualization.set('chartAxis', chartAxis);
-                    visualization.set('chartPadding', {left: 120});
-                    //visualization.set('chartPalette', {pattern: ['#1C766C', '#00140F', '#65B0AC', '#77B8BE', '#DBF6FD']});
-                    visualization.set('chartPalette', {pattern: ['#275D86', '#D1A932', '#D15F32', '#D14132']});
-                    visualization.set('rawData', rows);
-                })
-                .error(function(error) { console.error(error); });
+                        var chartData= {
+                            columns: columns,
+                            type: 'bar',
+                            groups: [
+                                visualization.columnNames
+                            ]
+                        };
+                        var chartAxis = {
+                            rotated: true,
+                            x: {
+                                type: 'category',
+                                categories: rows.map(function(row) { return row[visualization.categorizedBy]; })
+                            }
+                        };
+                        visualization.set('chartData', chartData);
+                        visualization.set('chartAxis', chartAxis);
+                        visualization.set('chartPadding', {left: 120});
+                        //visualization.set('chartPalette', {pattern: ['#1C766C', '#00140F', '#65B0AC', '#77B8BE', '#DBF6FD']});
+                        visualization.set('chartPalette', {pattern: ['#275D86', '#D1A932', '#D15F32', '#D14132']});
+                        visualization.set('rawData', rows);
+                    })
+                    .error(function(error) { console.error(error); });
+              }
         });
         return organization;
     }
